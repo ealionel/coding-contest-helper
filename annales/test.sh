31f3c3cd23a038e158f97c1860ff17ee1d9f7aef
Lionel EA <lionel.ea@utt.fr>
2019-03-21 19:17:46 +0100

    Init

---
+++ annales/test.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+echo 'wesh'
+script=$1
+input=$2
+output=$3
---
+++ battledev.js
@@ -0,0 +1,18 @@
+const readline = require('readline');
+
+const readline_object = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
+
+var input = [];
+
+readline_object.on("line", (value) => {
+	input.push(value);
+})
+readline_object.on("close", ContestResponse); 
+
+
+function ContestResponse(){
+	console.log('hello');
+}
